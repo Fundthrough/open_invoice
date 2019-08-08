5538823201abaf5d2c5111e52383fea8a7c524e5
Ahmed Refaat <ahmd.refat.twfik@gmail.com>
2019-08-08 13:15:45 -0400

    fix supplier init method

--- lib/open_invoice/entities/supplier.rb
+++ lib/open_invoice/entities/supplier.rb
@@ -2,7 +2,7 @@ module OpenInvoice
   module Entities
     class Supplier < Base
       def index(opts)
-        request(:get, "/suppliers/#{supplier_uuid}", opts)
+        request(:get, "/supplier/#{supplier_uuid}", opts)
       end
     end
   end
