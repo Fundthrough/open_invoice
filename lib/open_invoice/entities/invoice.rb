c9020ecdb1b9e596db95296a99f94e0ddb81bd4b
Ahmed Refaat <ahmd.refat.twfik@gmail.com>
2019-08-16 14:08:40 -0400

    update urls

--- lib/open_invoice/entities/invoice.rb
+++ lib/open_invoice/entities/invoice.rb
@@ -2,7 +2,7 @@ module OpenInvoice
   module Entities
     class Invoice < Base
       def index(opts = {})
-        request(:get, "/supplier/#{supplier_uuid}/invoices", opts)
+        request(:get, "/supplier/#{supplier_uuid}/invoices/page", opts)
       end
     end
   end
