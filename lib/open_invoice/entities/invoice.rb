b58adf1c25dab7ff0c771caae2c453252dfdf4f7
Mikalai Okun <mikalai@fundthrough.com>
2020-01-29 11:37:31 -0500

    Additions for invoice
    - Added attachments endpoints
    - Fixed typo

--- lib/open_invoice/entities/invoice.rb
+++ lib/open_invoice/entities/invoice.rb
@@ -1,8 +1,29 @@
 module OpenInvoice
   module Entities
     class Invoice < Base
+
       def index(opts = {})
         request(:get, "/supplier/#{supplier_uuid}/invoices/page", opts)
+      end
+
+      def attachments(invoice_id, opts = {})
+        request(:get, "/supplier/#{supplier_uuid}/invoices/#{invoice_id}/attachments", opts)
+      end
+
+      def download_attachment(invoice_id, attachment_id, file_path, opts)
+        attachment_url = "/supplier/#{@supplier_uuid}/invoices/#{invoice_id}/attachments/#{attachment_id}"
+        dirname = File.dirname(file_path)
+        opts.merge!({ :stream_body => true })
+        FileUtils.mkdir_p(dirname) unless File.directory?(dirname)
+        File.delete(file_path) if File.file?(file_path)
+        File.open(file_path, "w") do |file|
+            file.binmode
+            ::OpenInvoice::Http.get( attachment_url, opts) do |fragment|
+              file.write(fragment)
+            end
+        end
+    	  return file_path if File.file?(file_path)
+        nil
       end
     end
   end
