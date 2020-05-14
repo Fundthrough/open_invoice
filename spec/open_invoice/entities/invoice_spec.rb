ff149b4fa59057c3cdaad79d04f9d1a8977b17b9
Simi Akinrele <akinrelesimisola@yahoo.com>
2020-05-14 15:42:12 -0400

    use factory

--- spec/open_invoice/entities/invoice_spec.rb
+++ spec/open_invoice/entities/invoice_spec.rb
@@ -14,26 +14,7 @@ RSpec.describe OpenInvoice::Entities::Invoice do
   end
 
   describe "#history" do
-    let(:invoice_history) do
-      [
-        Hashie::Mash.new({
-          action: "Approve",
-          actionDate: "2020-02-03T12:31:12-07:00",
-          user: "the System"
-        }),
-        Hashie::Mash.new({
-          action: "Submit",
-          actionDate: "2020-02-03T12:31:11-07:00",
-          user: "Shayan Rahnama",
-          comments: "Please approve the invoice which would be used in testing purposes"
-        }),
-        Hashie::Mash.new({
-          action: "Save",
-          actionDate: "2020-02-03T12:20:34-07:00",
-          user: "Shayan Rahnama"
-        })
-      ]
-    end
+    let(:invoice_history) { create(:invoice_history) }
 
     let(:response) do
       Hashie::Mash.new(
