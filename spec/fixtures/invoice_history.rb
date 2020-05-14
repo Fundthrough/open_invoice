76d1ef0c1affbcdafb8a4178e38bd12efe0c45cd
Simi Akinrele <akinrelesimisola@yahoo.com>
2020-05-14 13:30:36 -0400

    fixture:

---
+++ spec/fixtures/invoice_history.rb
@@ -0,0 +1,18 @@
+h = [
+  {
+    action: "Approve",
+    actionDate: "2020-02-03T12:31:12-07:00",
+    user: "the System"
+    },
+    {
+      action: "Submit",
+      actionDate: "2020-02-03T12:31:11-07:00",
+      user: "Shayan Rahnama",
+      comments: "Please approve the invoice which would be used in testing purposes"
+    },
+    {
+      action: "Save",
+      actionDate: "2020-02-03T12:20:34-07:00",
+      user: "Shayan Rahnama"
+    }
+]
