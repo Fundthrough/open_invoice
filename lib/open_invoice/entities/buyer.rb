bb50b80ca5ce2bb646857ee1b0a527f4ac4316a6
mricher2000 <mricher@fundthrough.com>
2024-05-02 09:16:56 -0400

    update oi endpoint versions

--- lib/open_invoice/entities/buyer.rb
+++ lib/open_invoice/entities/buyer.rb
@@ -2,7 +2,7 @@ module OpenInvoice
   module Entities
     class Buyer < Base
       def index(opts = {})
-        request(:get, "/supplier/#{supplier_uuid}/buyers", opts)
+        request(:get, "/#{OpenInvoice::Configure::VERSION_2}/supplier/#{supplier_uuid}/buyers", opts)
       end
     end
   end
